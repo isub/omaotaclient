11a42aee148ae0a485cf273df39b0264dfc906df
Sabir <subbeer@gmail.com>
2019-01-09 10:08:22 +0300

    Распоряжение №5330-41 от 26.07.2018
    А так же перенос проекта в директорию проектов пользователя tenet и
    адаптация к его утилитам

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-﻿#ifdef WIN32
+#ifdef WIN32
 #	include <Windows.h>
 #endif
 #include "utils/config/config.h"
@@ -35,8 +35,8 @@ const char *g_pszAPNInet  = "<?xml version=\"1.0\"?><!DOCTYPE wap-provisioningdo
 const char *g_pszAPNInetHonor = "<?xml version=\"1.0\"?><!DOCTYPE wap-provisioningdoc PUBLIC \"-//WAPFORUM//DTD PROV 1.0//EN\" \"http://www.wapforum.org/DTD/prov.dtd\"><wap-provisioningdoc version=\"1.0\"><characteristic type=\"NAPDEF\"><parm name=\"NAPID\" value=\"NAP1\"/><parm name=\"NAME\" value=\"Letai Internet\"/><parm name=\"BEARER\" value=\"GSM-GPRS\"/><parm name=\"NAP-ADDRESS\" value=\"internet.letai.ru\"/><parm name=\"NAP-ADDRTYPE\" value=\"APN\"/></characteristic><characteristic type=\"APPLICATION\"><parm name=\"APPID\" value=\"w2\"/><parm name=\"NAME\" value=\"Letai Internet\"/><parm name=\"TO-NAPID\" value=\"NAP1\"/><characteristic type=\"RESOURCE\"><parm name=\"URI\" value=\"http://letai.ru\"/><parm name=\"NAME\" value=\"Letai Internet\"/><parm name=\"STARTPAGE\"/></characteristic></characteristic></wap-provisioningdoc>";
 const char *g_pszAPNMMS   = "<?xml version=\"1.0\"?><!DOCTYPE wap-provisioningdoc PUBLIC \"-//WAPFORUM//DTD PROV 1.0//EN\" \"http://www.wapforum.org/DTD/prov.dtd\"><wap-provisioningdoc version=\"1.0\"><characteristic type=\"BOOTSTRAP\"><parm name=\"NAME\" value=\"Letai MMS\"/></characteristic><characteristic type=\"PXLOGICAL\"><parm name=\"PROXY-ID\" value=\"Letai MMS\"/><parm name=\"NAME\" value=\"Letai MMS\"/><characteristic type=\"PXPHYSICAL\"><parm name=\"PHYSICAL-PROXY-ID\" value=\"PROXY 1\"/><parm name=\"PXADDR\" value=\"mmsc\"/><parm name=\"PXADDRTYPE\" value=\"ALPHA\"/><parm name=\"TO-NAPID\" value=\"NAP5\"/><characteristic type=\"PORT\"><parm name=\"PORTNBR\" value=\"8080\"></parm></characteristic></characteristic></characteristic><characteristic type=\"NAPDEF\"><parm name=\"NAPID\" value=\"NAP5\"/><parm name=\"NAME\" value=\"Letai MMS\"/><parm name=\"BEARER\" value=\"GSM-GPRS\"/><parm name=\"NAP-ADDRESS\" value=\"mms\"/><parm name=\"NAP-ADDRTYPE\" value=\"APN\"/></characteristic><characteristic type=\"APPLICATION\"><parm name=\"APPID\" value=\"w4\"/><parm name=\"NAME\" value=\"Letai MMS\"/><parm name=\"ADDR\" value=\"http://mmsc\"/><parm name=\"TO-PROXY\" value=\"Letai MMS\"/></characteristic></wap-provisioningdoc>";
 
-const char *g_pszTextInet = "Настройки мобильного интернета. В меню \"Настройки\" выберите пункт \"Сотовая связь\", \"Сотовая сеть передачи данных\" и в разделе \"Сотовые данные\" заполните параметр APN: internet.letai.ru";
-const char *g_pszTextMMS  = "Настройки MMS. В меню \"Настройки\" выберите пункт \"Сотовая связь\", \"Сотовая сеть передачи данных\" и в разделе \"MMS\" заполните параметры APN: mms; MMSC: https://mmsc:8002; MMS-прокси: mmsc:8080. Перезагрузите Ваше устройство.";
+const char *g_pszTextInet = "Настройки мобильного интернета. В меню \"Настройки\" выберите пункт \"Сотовая связь\", \"Сотовая сеть передачи данных\" и в разделе \"Сотовые данные\" заполните параметры: Имя: letai; APN: internet.letai.ru; Тип APN: default; Протокол роуминга APN: IPv4";
+const char *g_pszTextMMS  = "Настройки MMS. В меню \"Настройки\" выберите пункт \"Сотовая связь\", \"Сотовая сеть передачи данных\" и в разделе \"MMS\" заполните параметры: Имя: letai mms; APN: mms; MMSC: http://mmsc; Прокси-сервер: mmsc; Порт MMS: 8080; Тип APN: mms. Перезагрузите Ваше устройство.";
 
 const char *g_pszTextInetWnd = "Настройки мобильного интернета. В меню \"Настройки\" выберите пункт \"Сотовая сеть\" и в разделе \"Точки интернет-доступа\" заполните параметр APN(Точка доступа): internet.letai.ru";
 const char *g_pszTextMMSWnd  = "Настройки MMS. В меню \"Настройки\" выберите пункт \"Сотовая сеть\", в разделе \"Настройки SIM-карты\" добавьте новую точку доступа MMS и заполните параметры APN(Точка доступа): mms; MMS-центр: https://mmsc:8002; Порт MMS: 8080. Перезагрузите Ваше устройство.";
@@ -98,36 +98,54 @@ int main(int argc, char *argv[])
 	/* проверяем наличие параметра в командной строке */
 	if (argc < 2) {
 		iRetVal = -1;
-		LOG_E(coLog, "return code: '%d';", iRetVal);
+		UTL_LOG_E(coLog, "return code: '%d';", iRetVal);
 		return iRetVal;
 	}
 	/* загружаем конфигурацию */
 	iFnRes = coConf.LoadConf(argv[1]);
 	if (iFnRes) {
 		iRetVal = -2;
-		LOG_E(coLog, "return code: '%d';", iRetVal);
+		UTL_LOG_E(coLog, "return code: '%d';", iRetVal);
 		return iRetVal;
 	}
 	/* инициализируем логгер */
 	iFnRes = coConf.GetParamValue("log_file_mask", strConfParamValue);
 	if (iFnRes) {
 		iRetVal = -3;
-		LOG_E(coLog, "return code: '%d';", iRetVal);
+		UTL_LOG_E(coLog, "return code: '%d';", iRetVal);
 		return iRetVal;
 	}
 	iFnRes = coLog.Init(strConfParamValue.c_str());
 	if (iFnRes) {
 		iRetVal = -4;
-		LOG_E(coLog, "return code: '%d';", iRetVal);
+		UTL_LOG_E(coLog, "return code: '%d';", iRetVal);
 		return iRetVal;
 	}
 	/* инициализация сurl */
 	curl_global_init(0);
 	/* инициализируем пул подключений к БД */
-	iFnRes = db_pool_init(&coLog, &coConf);
+	std::string strDBUserName, strDBUserPswd, strDBDescr;
+	/* запрашиваем необходимые параметры */
+	pszParamName = "db_user";
+	iFnRes = coConf.GetParamValue(pszParamName, strDBUserName);
+	if (iFnRes) {
+		UTL_LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
+	}
+	pszParamName = "db_pswd";
+	iFnRes = coConf.GetParamValue(pszParamName, strDBUserPswd);
+	if (iFnRes) {
+		UTL_LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
+	}
+	pszParamName = "db_descr";
+	iFnRes = coConf.GetParamValue(pszParamName, strDBDescr);
+	if (iFnRes) {
+		UTL_LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
+	}
+  /* подключение к БД */
+	iFnRes = db_pool_init(&coLog, strDBUserName, strDBUserPswd, strDBDescr, 1);
 	if (iFnRes) {
 		iRetVal = -5;
-		LOG_E(coLog, "return code: '%d';", iRetVal);
+		UTL_LOG_E(coLog, "return code: '%d';", iRetVal);
 		return iRetVal;
 	}
 
@@ -141,27 +159,27 @@ int main(int argc, char *argv[])
 	pszParamName = "smsbox_username";
 	iFnRes = coConf.GetParamValue(pszParamName, strSMSBoxUserName);
 	if (iFnRes) {
-		LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
+		UTL_LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
 	}
 	pszParamName = "smsbox_userpswd";
 	iFnRes = coConf.GetParamValue(pszParamName, strSMSBoxUserPswd);
 	if (iFnRes) {
-		LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
+		UTL_LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
 	}
 	pszParamName = "smsbox_host";
 	iFnRes = coConf.GetParamValue(pszParamName, strSMSBoxHost);
 	if (iFnRes) {
-		LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
+		UTL_LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
 	}
 	pszParamName = "smsbox_url_ota";
 	iFnRes = coConf.GetParamValue(pszParamName, strSMSBoxOTAURL);
 	if (iFnRes) {
-		LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
+		UTL_LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
 	}
 	pszParamName = "smsbox_url_sms";
 	iFnRes = coConf.GetParamValue(pszParamName, strSMSBoxSMSURL);
 	if (iFnRes) {
-		LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
+		UTL_LOG_W(coLog, "parameter '%s' is not defined in configuration", pszParamName);
 	}
 
 	if (pcoDBConn) {
@@ -190,20 +208,20 @@ int main(int argc, char *argv[])
 					>> coIMSI
 					>> coProfile
 					>> coSettingsType;
-        LOG_N (coLog, "row selected: '%s'; '%s'; '%s'; '%s'", coMSISDN.v.c_str(), coIMSI.v.c_str(), coProfile.v.c_str(), coSettingsType.v.c_str());
+        UTL_LOG_N (coLog, "row selected: '%s'; '%s'; '%s'; '%s'", coMSISDN.v.c_str(), coIMSI.v.c_str(), coProfile.v.c_str(), coSettingsType.v.c_str());
         if (coMSISDN.is_null ()) {
-					LOG_E(coLog, "MSISDN not defined");
+					UTL_LOG_E(coLog, "MSISDN not defined");
 					goto delete_and_continue;
 				} else {
 					if (coMSISDN.v[0] != '+')
 						coMSISDN.v = '+' + coMSISDN.v;
 				}
 				if (coProfile.is_null()) {
-					LOG_E(coLog, "profile not defined");
+					UTL_LOG_E(coLog, "profile not defined");
 					goto delete_and_continue;;
 				}
 				if (coSettingsType.is_null()) {
-					LOG_E(coLog, "settings type not defined");
+					UTL_LOG_E(coLog, "settings type not defined");
 					goto delete_and_continue;;
 				}
 				if (0 == coProfile.v.compare("OMA CP NETWPIN")) {
@@ -217,7 +235,7 @@ int main(int argc, char *argv[])
 				} else if ( 0 == coProfile.v.compare( "HONOR" ) ) {
           eProfile = m_eHonor;
         } else {
-					LOG_E(coLog, "unsupported profile: '%s'", coProfile.v.c_str());
+					UTL_LOG_E(coLog, "unsupported profile: '%s'", coProfile.v.c_str());
 					goto delete_and_continue;;
 				}
 
@@ -228,7 +246,7 @@ int main(int argc, char *argv[])
 				} else if (0 == coSettingsType.v.compare("INTERNET+MMS")) {
 					eSettingsType = m_eInternetMMS;
 				} else {
-					LOG_E(coLog, "unsupported settings type: '%s'", coSettingsType.v.c_str());
+					UTL_LOG_E(coLog, "unsupported settings type: '%s'", coSettingsType.v.c_str());
 					goto delete_and_continue;;
 				}
 
@@ -265,11 +283,11 @@ int main(int argc, char *argv[])
 
         if (0 != strText.length()) {
 				  iFnRes = put_sms(coLog, strSMSBoxHost, strSMSBoxSMSURL, strSMSBoxUserName, strSMSBoxUserPswd, strHeader, coMSISDN.v, strText);
-				  LOG_N(coLog, "sms is sent with status '%d': '%s'; '%s'; '%s';", iFnRes, strHeader.c_str(), coMSISDN.v.c_str(), strText.c_str());
+				  UTL_LOG_N(coLog, "sms is sent with status '%d': '%s'; '%s'; '%s';", iFnRes, strHeader.c_str(), coMSISDN.v.c_str(), strText.c_str());
           try {
             coResult << coMSISDN << strHeader << strText << strSec << strPin << iFnRes;
           } catch (otl_exception &coExept) {
-            LOG_E (coLog, "code: '%d'; message: '%s'; query: '%s';", coExept.code, coExept.msg, coExept.stm_text);
+            UTL_LOG_E (coLog, "code: '%d'; message: '%s'; query: '%s';", coExept.code, coExept.msg, coExept.stm_text);
             coResult.clean();
           }
         }
@@ -297,11 +315,11 @@ int main(int argc, char *argv[])
           } else {
 						iFnRes = put_ota(coLog, strSMSBoxHost, strSMSBoxOTAURL, strSMSBoxUserName, strSMSBoxUserPswd, strHeader, coMSISDN.v, strText, strSec, strPin);
           }
-					LOG_N(coLog, "sms is sent with status '%d': '%s'; '%s'; '%s';", iFnRes, strHeader.c_str(), coMSISDN.v.c_str(), strText.c_str());
+					UTL_LOG_N(coLog, "sms is sent with status '%d': '%s'; '%s'; '%s';", iFnRes, strHeader.c_str(), coMSISDN.v.c_str(), strText.c_str());
           try {
             coResult << coMSISDN << strHeader << strText << strSec << strPin << iFnRes;
           } catch ( otl_exception &coExept ) {
-            LOG_E( coLog, "code: '%d'; message: '%s'; query: '%s';", coExept.code, coExept.msg, coExept.stm_text );
+            UTL_LOG_E( coLog, "code: '%d'; message: '%s'; query: '%s';", coExept.code, coExept.msg, coExept.stm_text );
             coResult.clean();
           }
         }
@@ -326,11 +344,11 @@ int main(int argc, char *argv[])
 						iFnRes = put_sms(coLog, strSMSBoxHost, strSMSBoxSMSURL, strSMSBoxUserName, strSMSBoxUserPswd, strHeader, coMSISDN.v, strText);
 					else
 						iFnRes = put_ota(coLog, strSMSBoxHost, strSMSBoxOTAURL, strSMSBoxUserName, strSMSBoxUserPswd, strHeader, coMSISDN.v, strText, strSec, strPin);
-					LOG_N(coLog, "sms is sent with status '%d': '%s'; '%s'; '%s';", iFnRes, strHeader.c_str(), coMSISDN.v.c_str(), strText.c_str());
+					UTL_LOG_N(coLog, "sms is sent with status '%d': '%s'; '%s'; '%s';", iFnRes, strHeader.c_str(), coMSISDN.v.c_str(), strText.c_str());
           try {
   					coResult << coMSISDN << strHeader << strText << strSec << strPin << iFnRes;
           } catch (otl_exception &coExept) {
-            LOG_E (coLog, "code: '%d'; message: '%s'; query: '%s';", coExept.code, coExept.msg, coExept.stm_text);
+            UTL_LOG_E (coLog, "code: '%d'; message: '%s'; query: '%s';", coExept.code, coExept.msg, coExept.stm_text);
             coResult.clean();
           }
 				}
@@ -349,11 +367,11 @@ int main(int argc, char *argv[])
             break;
           }
 					iFnRes = put_sms(coLog, strSMSBoxHost, strSMSBoxSMSURL, strSMSBoxUserName, strSMSBoxUserPswd, strHeader, coMSISDN.v, strText);
-					LOG_N(coLog, "sms is sent with status '%d': '%s'; '%s'; '%s';", iFnRes, strHeader.c_str(), coMSISDN.v.c_str(), strText.c_str());
+					UTL_LOG_N(coLog, "sms is sent with status '%d': '%s'; '%s'; '%s';", iFnRes, strHeader.c_str(), coMSISDN.v.c_str(), strText.c_str());
           try {
             coResult << coMSISDN << strHeader << strText << strSec << strPin << iFnRes;
           } catch (otl_exception &coExept) {
-            LOG_E (coLog, "code: '%d'; message: '%s'; query: '%s';", coExept.code, coExept.msg, coExept.stm_text);
+            UTL_LOG_E (coLog, "code: '%d'; message: '%s'; query: '%s';", coExept.code, coExept.msg, coExept.stm_text);
             coResult.clean();
           }
         }
@@ -361,10 +379,10 @@ int main(int argc, char *argv[])
 				coDelete << coRowId;
 			}
 		} catch (otl_exception &coExept) {
-			LOG_E(coLog, "code: '%d'; message: '%s'; query: '%s';", coExept.code, coExept.msg, coExept.stm_text);
+			UTL_LOG_E(coLog, "code: '%d'; message: '%s'; query: '%s';", coExept.code, coExept.msg, coExept.stm_text);
 		}
 	} else {
-		LOG_E(coLog, "can't get DB connection");
+		UTL_LOG_E(coLog, "can't get DB connection");
 	}
 
 	if (pcoDBConn) {
@@ -426,14 +444,14 @@ int put_sms(
 		/* добавляем список заголовков в HTTP-запрос */
 		curlCode = curl_easy_setopt(pCurl, CURLOPT_HTTPHEADER, psoList);
 		if (curlCode) {
-			LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_HTTPHEADER", curlCode, mcError);
+			UTL_LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_HTTPHEADER", curlCode, mcError);
 			iRetVal = -100;
 			break;
 		}
 		/* задаем User-Agent */
 		curlCode = curl_easy_setopt(pCurl, CURLOPT_USERAGENT, "smsboxclient/0.1");
 		if (curlCode) {
-			LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_USERAGENT", curlCode, mcError);
+			UTL_LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_USERAGENT", curlCode, mcError);
 			iRetVal = -101;
 			break;
 		}
@@ -452,32 +470,32 @@ int put_sms(
 		strURL += strParam;
 		curlCode = curl_easy_setopt(pCurl, CURLOPT_URL, strURL.c_str());
 		if (curlCode) {
-			LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_URL", curlCode, mcError);
+			UTL_LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_URL", curlCode, mcError);
 			iRetVal = -103;
 			break;
 		}
 		/* задаем тип запроса GET */
 		curlCode = curl_easy_setopt(pCurl, CURLOPT_HTTPGET, 1L);
 		if (curlCode) {
-			LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_HTTPGET", curlCode, mcError);
+			UTL_LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_HTTPGET", curlCode, mcError);
 			iRetVal = -104;
 			break;
 		}
-		LOG_N(p_coLog, "try to send request: '%s';", strURL.c_str());
+		UTL_LOG_N(p_coLog, "try to send request: '%s';", strURL.c_str());
 		curlCode = curl_easy_perform(pCurl);
 		if (curlCode) {
-			LOG_E(p_coLog, "can't execute request: error code: '%d'; description: '%s'; url: '%s';", curlCode, mcError, strURL.c_str());
+			UTL_LOG_E(p_coLog, "can't execute request: error code: '%d'; description: '%s'; url: '%s';", curlCode, mcError, strURL.c_str());
 			iRetVal = -105;
 			break;
 		}
 		long lResulCode;
 		curlCode = curl_easy_getinfo(pCurl, CURLINFO_RESPONSE_CODE, &lResulCode);
 		if (curlCode) {
-			LOG_E(p_coLog, "can't retrieve request result code: error code: '%d'; description: '%s';", curlCode, mcError);
+			UTL_LOG_E(p_coLog, "can't retrieve request result code: error code: '%d'; description: '%s';", curlCode, mcError);
 			iRetVal = -106;
 			break;
 		}
-		LOG_N(p_coLog, "request is sent successfully: response code: '%u';", lResulCode);
+		UTL_LOG_N(p_coLog, "request is sent successfully: response code: '%u';", lResulCode);
 		/* операция завершена успешно */
 		iRetVal = lResulCode;
 	} while (0);
@@ -518,7 +536,7 @@ int conv_string_to_ucs2(
 	tIConv = iconv_open("UCS-2BE", "UTF-8");
 	if (tIConv == (iconv_t)-1) {
 		iRetVal = errno;
-		LOG_E(p_coLog, "can't create iconv descriptor; error: code: '%d';", iRetVal);
+		UTL_LOG_E(p_coLog, "can't create iconv descriptor; error: code: '%d';", iRetVal);
 		return iRetVal;
 	}
 
@@ -537,7 +555,7 @@ int conv_string_to_ucs2(
 	stStrLen = iconv(tIConv, (char**)(&pszInBuf), &stStrLen, &pszOut, &stResultSize);
 	if (stStrLen == (size_t)-1) {
 		iRetVal = errno;
-		LOG_E(p_coLog, "iconv conversion error: code: '%d';", iRetVal);
+		UTL_LOG_E(p_coLog, "iconv conversion error: code: '%d';", iRetVal);
 		goto clean_and_exit;
 	}
 
@@ -593,14 +611,14 @@ int put_ota(
 		/* добавляем список заголовков в HTTP-запрос */
 		curlCode = curl_easy_setopt(pCurl, CURLOPT_HTTPHEADER, psoList);
 		if (curlCode) {
-			LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_HTTPHEADER", curlCode, mcError);
+			UTL_LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_HTTPHEADER", curlCode, mcError);
 			iRetVal = -100;
 			break;
 		}
 		/* задаем User-Agent */
 		curlCode = curl_easy_setopt(pCurl, CURLOPT_USERAGENT, "omaotaclient/0.1");
 		if (curlCode) {
-			LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_USERAGENT", curlCode, mcError);
+			UTL_LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_USERAGENT", curlCode, mcError);
 			iRetVal = -101;
 			break;
 		}
@@ -625,32 +643,32 @@ int put_ota(
 		strURL += strParam;
 		curlCode = curl_easy_setopt(pCurl, CURLOPT_URL, strURL.c_str());
 		if (curlCode) {
-			LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_URL", curlCode, mcError);
+			UTL_LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_URL", curlCode, mcError);
 			iRetVal = -103;
 			break;
 		}
 		/* задаем тип запроса GET */
 		curlCode = curl_easy_setopt(pCurl, CURLOPT_HTTPGET, 1L);
 		if (curlCode) {
-			LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_HTTPGET", curlCode, mcError);
+			UTL_LOG_E(p_coLog, "can't set option '%s'; error: code: '%d'; description: '%s';", "CURLOPT_HTTPGET", curlCode, mcError);
 			iRetVal = -104;
 			break;
 		}
-		LOG_N(p_coLog, "try to send request: '%s';", strURL.c_str());
+		UTL_LOG_N(p_coLog, "try to send request: '%s';", strURL.c_str());
 		curlCode = curl_easy_perform(pCurl);
 		if (curlCode) {
-			LOG_E(p_coLog, "can't execute request: error code: '%d'; description: '%s'; url: '%s';", curlCode, mcError, strURL.c_str());
+			UTL_LOG_E(p_coLog, "can't execute request: error code: '%d'; description: '%s'; url: '%s';", curlCode, mcError, strURL.c_str());
 			iRetVal = -105;
 			break;
 		}
 		long lResulCode;
 		curlCode = curl_easy_getinfo(pCurl, CURLINFO_RESPONSE_CODE, &lResulCode);
 		if (curlCode) {
-			LOG_E(p_coLog, "can't retrieve request result code: error code: '%d'; description: '%s';", curlCode, mcError);
+			UTL_LOG_E(p_coLog, "can't retrieve request result code: error code: '%d'; description: '%s';", curlCode, mcError);
 			iRetVal = -106;
 			break;
 		}
-		LOG_N(p_coLog, "request is sent successfully: response code: '%u';", lResulCode);
+		UTL_LOG_N(p_coLog, "request is sent successfully: response code: '%u';", lResulCode);
 		/* операция завершена успешно */
 		iRetVal = lResulCode;
 	} while (0);
